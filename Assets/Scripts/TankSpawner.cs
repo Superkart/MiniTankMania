5115e51e801b987f0728a1ba9203b076bd88345d
Karthik Ragi <karthik@xarpie.com>
2022-11-24 03:04:55 +0530

    Reuploading the correct project files to github

---
+++ Assets/Scripts/TankSpawner.cs
@@ -0,0 +1,15 @@
+
+using UnityEngine;
+
+public class TankSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject tankPrefab;
+
+
+    private void Start()
+    {
+        Instantiate(tankPrefab, transform.position, Quaternion.identity);
+       
+    }
+
+}
